c8fba8e6e591ca2534b2f4392d9576343f80a330
Maria <maria123321borovikova@gmail.com>
2023-03-31 18:38:17 +0200

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,54 @@
 import { gql } from 'graphql-request';
-import { client } from '../lib/client';
-import Link from 'next/link';
-import styles from './Home.module.css';
-import { MainTitle } from '@/components/MainTitle';
+import { client } from '@/lib/client';
+import styles from './InfoAboutMe.module.css';
 
-type Project = {
-  id: string;
-  title: string;
-  description: string;
-  skills: string;
-  image: {
+type AboutMe = {
+  info: string;
+  picture: {
     url: string;
   };
 };
 
-type AllProjectsQueryResult = {
-  all_Projects: Project[];
+type AboutMeQueryResult = {
+  aboutMes: AboutMe[];
 };
 
-const AllProjects = gql`
-  query AllProjects {
-    all_Projects {
-      id
-      title
-      description
-      skills
-      image {
+const AboutMe = gql`
+  query MyQuery {
+    aboutMes {
+      info
+      picture {
         url
       }
     }
   }
 `;
 
-export default async function Home() {
-  const { all_Projects }: AllProjectsQueryResult = await client.request(
-    AllProjects
-  );
+export default async function InfoAboutMe() {
+  const { aboutMes }: AboutMeQueryResult = await client.request(AboutMe);
 
-  const firstThreeProjects = all_Projects.slice(0, 3);
+  const description = aboutMes[0].info;
+  const sentences = description.split(/[.?!]/);
+  const thirdSentence = sentences[2].trim();
+  const fifthSentence = sentences[4].trim();
+  const descriptionWithSpans = description.replace(thirdSentence, `<span class="${styles.thirdSentence}">${thirdSentence}</span>`);
+  const finalDescription = descriptionWithSpans.replace(fifthSentence, `<span class="${styles.fifthSentence}">${fifthSentence}</span>`);
 
   return (
-    <main>
-      <div className={styles.title}>
-        <MainTitle />
-        <div className={styles.grid}>
-          {firstThreeProjects.map((project: Project, projectIndex: number) => {
-            return (
-              <div key={projectIndex} className={styles.project}>
-                <Link href={`/projects/${project.id}`}>
-                  <div><h2>{project.title}</h2></div>
-                </Link>
-                <img src={project.image.url} alt={project.title} />
-              </div>
-            );
-          })}
+    <div className={styles.container}>
+      {aboutMes.map((aboutMe: AboutMe, index: number) => (
+        <div key={index} className={styles.row}>
+          <div className={styles.imageContainer}>
+            <img src={aboutMe.picture.url} alt="Picture of me" className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>Welcome to my portfolio.</h1>
+            <p className={styles.description} dangerouslySetInnerHTML={{ __html: finalDescription }}></p>
+          </div>
         </div>
-      </div>
-    </main>
+      ))}
+    </div>
   );
 }
+
+
